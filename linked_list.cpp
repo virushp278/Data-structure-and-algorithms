db6ef6d348467e20e5f79ea241ae06ba1ebc122f
virushp278 <95963827+virushp278@users.noreply.github.com>
2025-04-02 18:49:34 +0000

    delete Node

--- linked_list.cpp
+++ linked_list.cpp
@@ -6,9 +6,22 @@ class Node {
     int data;
     Node* next;
 
+
+    //constructor
     Node(int data){
         this -> data = data;
         this -> next = NULL;
+    }
+
+    //deconstructor
+    ~Node(){
+        int value = this-> data;
+        //memory free
+        if(this-> next !=NULL){
+            delete next;
+            this -> next = NULL;
+        }
+        cout << "memory is free for node with data " << value << endl;
     }
 };
 
@@ -53,6 +66,36 @@ void insertAtPosition(Node* &tail ,Node* &head,int position,int d){
     }
 
 } 
+
+void deleteNode(int position, Node* & head){
+
+    //deleting first or start node
+    if(position==1){
+        Node* temp = head;
+        head= head->next;
+        //memory free start Node
+        temp -> next = NULL;
+        delete temp;
+    }
+
+    //deleting at any middle or last node
+    else{
+        Node* curr = head;
+        Node* prev = NULL;
+        
+        int cnt = 1;
+        while(cnt<=position){
+            prev = curr;
+            curr= curr-> next;
+            cnt++;
+
+        } 
+        prev -> next = curr -> next;
+        curr -> next = NULL;
+        delete curr;
+    }
+
+}
 
 void print(Node* &head){
     Node* temp = head;
